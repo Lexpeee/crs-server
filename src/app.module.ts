c094a107be1611da989ebc1bdcffabc8046e776d
Alexis Pineda <ixspnd.ap@gmail.com>
2025-12-17 10:16:30 +0800

    chore: directory naming adjustments

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,9 @@ import { AppService } from './app.service';
 import { DriverModule } from './modules/driver/driver.module';
 import { VehicleModule } from './modules/vehicle/vehicle.module';
 import { RentModule } from './modules/rent/rent.module';
-import { RentLogsModule } from './modules/record-logs/rent.logs/rent.logs.module';
-import { FuelModule } from './modules/records/fuel/fuel.module';
-import { VehicleItemsModule } from './modules/records/vehicle-items/vehicle-items.module';
+import { RentLogsModule } from './modules/audit/rent.logs/rent.logs.module';
+import { FuelModule } from './modules/reports/fuel/fuel.module';
+import { VehicleItemsModule } from './modules/reports/vehicle-items/vehicle-items.module';
 
 @Module({
   imports: [
--- src/modules/rent/rent.module.ts
+++ src/modules/rent/rent.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { RentService } from './rent.service';
 import { RentResolver } from './rent.resolver';
-import { RentLogsService } from '../record-logs/rent.logs/rent.logs.service';
+import { RentLogsService } from '../audit/rent.logs/rent.logs.service';
 
 @Module({
   providers: [RentResolver, RentService, RentLogsService],
