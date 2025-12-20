c7d57f7ca6cfae1de7869b5fd00add95f4ccf27f
Alexis Pineda <ixspnd.ap@gmail.com>
2025-12-21 04:08:12 +0800

    fix: mutation property types; add password logic

--- src/modules/accounts/accounts.service.ts
+++ src/modules/accounts/accounts.service.ts
@@ -1,16 +1,41 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAccountInput } from './entities/mutation';
 import { QUERY_VERSION, RESPONSE_STATUS } from 'src/helpers/_enums';
+import MAccount from './db/MAccount';
+import { CreateAccountInput } from './entities/mutation';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AccountsService {
-  create(data: CreateAccountInput) {
-    const newAccount = data;
-    return {
-      data: newAccount,
-      status: RESPONSE_STATUS.OK,
-      version: QUERY_VERSION.v1,
-    };
+  async create(data: CreateAccountInput) {
+    try {
+      const account = await MAccount.findOne({
+        $or: [{ email: data.email }, { username: data.username }],
+      }).lean();
+      if (account) {
+        return {
+          data: null,
+          status: RESPONSE_STATUS.ERROR,
+          version: QUERY_VERSION.v1,
+        };
+      }
+
+      const newData = data;
+      const hashedPassword = (await bcrypt.hash(data.password, 10)) as string;
+      newData.password = hashedPassword;
+
+      const newAccount = await new MAccount(newData).save();
+      return {
+        data: newAccount,
+        status: RESPONSE_STATUS.OK,
+        version: QUERY_VERSION.v1,
+      };
+    } catch (error) {
+      return {
+        error: error.message,
+        status: RESPONSE_STATUS.OK,
+        version: QUERY_VERSION.v1,
+      };
+    }
   }
 
   login(data: CreateAccountInput) {
--- src/modules/accounts/entities/mutation/index.ts
+++ src/modules/accounts/entities/mutation/index.ts
@@ -1,15 +1,25 @@
-import { InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class CreateAccountInput {
+  @Field(() => String)
   email: string;
+
+  @Field(() => String)
   username: string;
+
+  @Field(() => String)
   password: string;
+
+  @Field(() => String)
   accountType: string;
 }
 
 @InputType()
 export class LoginAccountInput {
+  @Field(() => String)
   email: string;
+
+  @Field(() => String)
   password: string;
 }
